87448a0e0f7630ec38b6e29103649f5bc1ede793
kons <kons.koehler@gmail.com>
2021-09-16 11:12:20 +0200

    Implement mapping challenge

--- Backend/mapping-kotlin/src/main/kotlin/com/mhp/coding/challenges/mapping/mappers/ArticleMapper.kt
+++ Backend/mapping-kotlin/src/main/kotlin/com/mhp/coding/challenges/mapping/mappers/ArticleMapper.kt
@@ -1,15 +1,47 @@
 package com.mhp.coding.challenges.mapping.mappers
 
 import com.mhp.coding.challenges.mapping.models.db.Article
+import com.mhp.coding.challenges.mapping.models.db.Image
+import com.mhp.coding.challenges.mapping.models.db.ImageSize
+import com.mhp.coding.challenges.mapping.models.db.blocks.GalleryBlock
+import com.mhp.coding.challenges.mapping.models.db.blocks.ImageBlock
+import com.mhp.coding.challenges.mapping.models.db.blocks.TextBlock
+import com.mhp.coding.challenges.mapping.models.db.blocks.VideoBlock
 import com.mhp.coding.challenges.mapping.models.dto.ArticleDto
+import com.mhp.coding.challenges.mapping.models.dto.ImageDto
+import com.mhp.coding.challenges.mapping.models.dto.blocks.GalleryBlockDto
+import com.mhp.coding.challenges.mapping.models.dto.blocks.ImageBlockDto
+import com.mhp.coding.challenges.mapping.models.dto.blocks.TextBlockDto
+import com.mhp.coding.challenges.mapping.models.dto.blocks.VideoBlockDto
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
 class ArticleMapper {
     fun map(article: Article?): ArticleDto {
-        //TODO
-        return ArticleDto(0, "", "", "", emptyList())
+        return if (article != null) {
+            val articleBlockDtoList = article.blocks.map { block ->
+                when (block) {
+                    is GalleryBlock -> GalleryBlockDto(block.images.map { mapImage(it) }, block.sortIndex)
+                    is ImageBlock -> ImageBlockDto(mapImage(block.image), block.sortIndex)
+                    is TextBlock -> TextBlockDto(block.text, block.sortIndex)
+                    is VideoBlock -> VideoBlockDto(block.url, block.type, block.sortIndex)
+                    else -> throw IllegalStateException("ArticleBlock Type unknown.")
+                }
+            }.sortedBy { it.sortIndex }
+            article.let { ArticleDto(it.id, it.title, it.description ?: "", it.author ?: "", articleBlockDtoList) }
+        } else {
+            ArticleDto(0, "", "", "", emptyList())
+        }
+    }
+
+    fun mapImage(image: Image?): ImageDto {
+        return if (image != null) {
+            ImageDto(image.id, image.url, image.imageSize)
+        } else {
+            // Since the ArticleRepository.kt always returns images as null (cf. line 74), unfortunately, this is obligatory.
+            ImageDto(0, "", ImageSize.SMALL)
+        }
     }
 
     // Not part of the challenge / Nicht Teil dieser Challenge.
--- Backend/mapping-kotlin/src/main/kotlin/com/mhp/coding/challenges/mapping/services/ArticleService.kt
+++ Backend/mapping-kotlin/src/main/kotlin/com/mhp/coding/challenges/mapping/services/ArticleService.kt
@@ -3,7 +3,9 @@ package com.mhp.coding.challenges.mapping.services
 import com.mhp.coding.challenges.mapping.repositories.ArticleRepository
 import com.mhp.coding.challenges.mapping.mappers.ArticleMapper
 import com.mhp.coding.challenges.mapping.models.dto.ArticleDto
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
 
 @Service
 class ArticleService(
@@ -11,14 +13,21 @@ class ArticleService(
 ) {
     fun list(): List<ArticleDto> {
         val articles = ArticleRepository.all()
-        //TODO
-        return emptyList()
+        return articles.map { mapper.map(it) }
     }
 
     fun articleForId(id: Long): ArticleDto {
         val article = ArticleRepository.findBy(id)
-        //TODO
-        return ArticleDto(0, "", "", "", emptyList())
+
+        // Since the ArticleRepository.kt always returns valid articles, I am not sure, how an unknown article would be
+        // represented. Assuming null here, however I am aware that it can never be null with the current
+        // implementation of ArticleRepository.kt.
+        // I'd much rather like to return 404 in the ArticleController.kt. However, I guess that I am not supposed to alter it.
+        if (article == null) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find article")
+        } else {
+            return article.let { mapper.map(it) }
+        }
     }
 
     fun create(articleDto: ArticleDto): ArticleDto {
